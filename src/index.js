1c764e063d9f0e6534e1639aa5d3e45eebabc406
Karthik Jagadeesh <kartu8@gmail.com>
2017-12-18 13:57:59 +0530

    Add tree root

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import "babel-polyfill";
 import p5 from "p5";
 
-import Tree from "./Tree";
+import { Tree } from "./Tree";
 
 const app = p => {
-
   p.setup = _ => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     const whiteColor = 255;
     p.background(whiteColor);
+
+    const rootStart = p.createVector(p.width / 2, p.height);
+    const rootEnd = p.createVector(p.width / 2, p.height - 100);
+    const root = new Tree(p, rootStart, rootEnd);
+    root.show();
+    const branchRight = root.branchRight();
+    branchRight.show();
+    const branchLeft = root.branchLeft();
+    branchLeft.show();
   };
 
-  p.draw = _ => {};
+  p.draw = _ => {
+    // p.background(255);
+  };
 };
 
 const appInstance = new p5(app);
--- src/tree.js
+++ src/tree.js
@@ -1,9 +1,39 @@
 class Tree {
-  constructor(p) {
+  constructor(p, rootStart, rootEnd) {
     this._p = p;
+    this.start = rootStart;
+    this.end = rootEnd;
   }
 
-  show() {}
+  show() {
+    const p = this._p;
+
+    p.line(this.start.x, this.start.y, this.end.x, this.end.y);
+  }
+
+  branchRight() {
+    const p = this._p;
+
+    const start = p.createVector(this.end.x, this.end.y);
+    const end = this.end.copy();
+    end.sub(this.start);
+    end.rotate(p.PI / 4);
+    end.add(this.end);
+    const rightBranch = new Tree(p, start, end);
+    return rightBranch;
+  }
+
+  branchLeft() {
+    const p = this._p;
+
+    const start = p.createVector(this.end.x, this.end.y);
+    const end = this.end.copy();
+    end.sub(this.start);
+    end.rotate(-p.PI / 4);
+    end.add(this.end);
+    const leftBranch = new Tree(p, start, end);
+    return leftBranch;
+  }
 }
 
 export { Tree };
